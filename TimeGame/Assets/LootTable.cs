e3a9116077ac1066c1ac7c34695e041850920afc
Franz <Franzzz1357@googlemail.com>
2020-05-07 19:06:27 +0100

    tom

---
+++ TimeGame/Assets/LootTable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loottable : MonoBehaviour
+{
+    public int[] table = {
+        60,
+        30,
+        10
+    };
+    // Start is called before the first frame update
+    public int total;
+    private void Start()
+    {
+        /*foreach (var item in table)
+        {
+            total += item;
+        }*/
+    }
+}
