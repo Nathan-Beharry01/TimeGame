c067f3188f9763d2acb48b0c3628795e4049c982
nathan <nathan.beharry@uk.fujitsu.com>
2020-05-07 19:06:17 +0100

    gun  stuff

---
+++ TimeGame/Assets/BeastPlayer/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject[] loadout;
+    public Transform weaponParent;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    void Equip(int weaponID)
+    {
+        GameObject t_newEquiptment = Instantiate(loadout[weaponID], weaponParent.position, weaponParent.rotation, weaponParent) as GameObject;
+    }
+}
